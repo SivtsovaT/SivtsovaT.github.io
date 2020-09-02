1665b92390327b4d3c72df3e072829abfd100d83
SivtsovaT <66840978+SivtsovaT@users.noreply.github.com>
2020-09-02 10:26:06 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+    
+    $('ul.catalog_tabs').on('click', 'li:not(.catalog_tab_active)', function() {
+        $(this)
+          .addClass('catalog_tab_active').siblings().removeClass('catalog_tab_active')
+          .closest('div.container').find('div.catalog_content').removeClass('catalog_content_active').eq($(this).index()).addClass('catalog_content_active');
     });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item_content').eq(i).toggleClass('catalog-item_content_active');
+                $('.catalog-item_list').eq(i).toggleClass('catalog-item_list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item_button');
+    toggleSlide('.catalog-item_back');
+});
