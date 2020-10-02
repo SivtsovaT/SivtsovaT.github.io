a62154f8d8fd3431863e223babb9de9ec4900bda
SivtsovaT <66840978+SivtsovaT@users.noreply.github.com>
2020-10-02 10:27:08 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,36 +1,100 @@
-$(document).ready(function(){
-    $('.carousel_inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+window.addEventListener('DOMContentLoaded', function() {
+
     
-    $('ul.catalog_tabs').on('click', 'li:not(.catalog_tab_active)', function() {
-        $(this)
-          .addClass('catalog_tab_active').siblings().removeClass('catalog_tab_active')
-          .closest('div.container').find('div.catalog_content').removeClass('catalog_content_active').eq($(this).index()).addClass('catalog_content_active');
-    });
+    
+	let tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+        
+        function hideTabContent() {
+            tabsContent.forEach( item =>{item.style.display='none'});
+            tabs.forEach( item =>{item.classList.remove('tabheader__item_active')});
+          }
+          function showTabContent(i=0){
+              tabsContent[i].style.display = 'block';
+              tabs[i].classList.add('tabheader__item_active');
+          } 
 
-    function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item_content').eq(i).toggleClass('catalog-item_content_active');
-                $('.catalog-item_list').eq(i).toggleClass('catalog-item_list_active');
-            })
+
+
+          hideTabContent();
+         showTabContent();
+      tabsParent.addEventListener('click',function(event){
+      const target = event.target;
+    if(target && target.classList.contains('tabheader__item')){
+        tabs.forEach((item,i) => {
+            if(target == item){
+                hideTabContent();
+                showTabContent(i);
+            }
         });
-    };
+    }
+});
 
-    toggleSlide('.catalog-item_button');
-    toggleSlide('.catalog-item_back');
+
+const deadline = '2020-10-15';
+
+function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor( (t/(1000*60*60*24)) ),
+        seconds = Math.floor( (t/1000) % 60 ),
+        minutes = Math.floor( (t/1000/60) % 60 ),
+        hours = Math.floor( (t/(1000*60*60) % 24) );
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+function getZero (num) {
+    if(num >= 0 && num < 10){
+        return '0' + num;
+    } else{return num;
+    }
+}
+
+
+ function setClock (selector,endtime){
+     const timer = document.querySelector(selector),
+     days = timer.querySelector('#days'),
+     hours = timer.querySelector('#hours'),
+     minutes = timer.querySelector('#minutes'),
+     seconds = timer.querySelector('#seconds'),
+     timeInterval = setInterval(updateClock,1000);
+     updateClock();
+
+function updateClock(){
+    const t = getTimeRemaining(endtime);
+       days.innerHTML = getZero(t.days);
+       hours.innerHTML = getZero(t.hours);
+       minutes.innerHTML = getZero(t.minutes);
+       seconds.innerHTML = getZero(t.seconds);
+       if(t.total <=0 ) {
+           clearInterval(timeInterval);
+       }
+}
+ }
+ setClock('.timer',deadline);
+
+
+ const modalTrigger = document.querySelector('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+modalTrigger.addEventListener('click', () => {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+
+});
+
+modalCloseBtn.addEventListener('click', () => {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+});
+
 });
